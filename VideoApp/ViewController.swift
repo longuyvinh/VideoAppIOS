0247a55f2021f63e3f146eeaeb537714903a5fa6
macbook <macbook@macbooks-MacBook-Air.local>
2016-04-13 16:05:44 +0700

    upload code ios

---
+++ VideoApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  VideoApp
+//
+//  Created by macbook on 4/7/16.
+//  Copyright © 2016 magic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.labelTitle.font = UIFont(name:"Amatic-Bold", size: 30)
+        /*
+        for family: String in UIFont.familyNames()
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNamesForFamilyName(family)
+            {
+                print("== \(names)")
+            }
+        }*/
+        
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        print("width: \(screenWidth) - hight: \(screenHeight) ")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
