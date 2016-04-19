1dc4f27a7a1220e107d76270ff532552a428a96b
macbook <macbook@macbooks-MacBook-Air.local>
2016-04-19 22:25:23 +0700

    update 2 view watched and watch later

---
+++ VideoApp/vcWachedList.swift
@@ -0,0 +1,38 @@
+//
+//  vcWachedList.swift
+//  VideoApp
+//
+//  Created by macbook on 4/19/16.
+//  Copyright © 2016 magic. All rights reserved.
+//
+
+import UIKit
+
+class vcWachedList: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func actionBack(sender: AnyObject) {
+        self.navigationController!.popViewControllerAnimated(true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
